28c5eba6815a9be4d449ded9da766c0e6171f621
leonid <mr.konchulizov@mail.ru>
2020-12-31 14:13:37 +0300

    create a list of tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="home pa-7">
-    <h1>Todo Page</h1>
+  <div class="home">
+    <v-list two-line flat>
+      <v-list-item>
+        <template v-slot:default="{ active }">
+          <v-list-item-action>
+            <v-checkbox :input-value="active" color="primary"></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Celebrate New Year</v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
