d44653f5d9a05f522bf7dff1670de5a8556f0996
leonid <mr.konchulizov@mail.ru>
2020-12-31 15:36:02 +0300

    no Tasks state

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -14,6 +14,7 @@
 		></v-text-field>
 
     <v-list
+			v-if="tasks.length"
 			class="pt-0"
       flat
     >
@@ -53,6 +54,24 @@
 				<v-divider></v-divider>
 			</div>			
     </v-list>
+
+		<div
+		  v-else
+			class="no-tasks"
+		>
+			<v-icon
+			  color="primary"
+			  size="100"
+			>
+				mdi-check
+			</v-icon>
+			<div
+			  class="text-h5 primary--text"
+			>
+				No tasks
+			</div>
+		</div>
+
   </div>
 </template>
 
@@ -106,3 +125,12 @@ export default {
 	},
 }
 </script>
+
+<style lang="sass" scoped>
+.no-tasks
+	position: absolute
+	left: 50%
+	top: 50%
+	transform: translate(-50%, -50%)
+	opacity: .5
+</style>
